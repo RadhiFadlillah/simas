98827075212930ee84db4a5d4bdb6b403f931263
Radhi Fadlillah <m.radhi.f@gmail.com>
2016-12-20 15:47:08 +0700

    Initial back end

---
+++ back-end.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/xeodou/go-sqlcipher"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const dbSource = "root:@/simas"
+
+type BackEnd struct {
+	DB         *sqlx.DB
+	PortNumber int
+}
+
+func NewBackEnd(port int) BackEnd {
+	backEnd := BackEnd{
+		DB:         sqlx.MustConnect("mysql", dbSource),
+		PortNumber: port,
+	}
+
+	backEnd.generateAdmin()
+
+	return backEnd
+}
+
+func (backend *BackEnd) ServeApp() {
+	// Create router
+	router := httprouter.New()
+
+	// Set panic handler
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, arg interface{}) {
+		http.Error(w, fmt.Sprint(arg), 500)
+	}
+
+	// Serve app
+	log.Printf("Serve app in port %d\n", backend.PortNumber)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", backend.PortNumber), router))
+}
+
+func (backend *BackEnd) Close() {
+	backend.DB.Close()
+}
+
+func (backend *BackEnd) generateAdmin() {
+	// If there are no existing account, create new admin
+	var nAccount int
+	err := backend.DB.Get(&nAccount, "SELECT COUNT(*) FROM account")
+	checkError(err)
+
+	if nAccount == 0 {
+		password := []byte("admin")
+		hashedPassword, err := bcrypt.GenerateFromPassword(password, 10)
+		checkError(err)
+
+		backend.DB.MustExec(`INSERT INTO account 
+			(email, nama, password, admin) VALUES (?, ?, ?, ?)`,
+			"admin@simas", "Administrator", hashedPassword, 1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+)
+
+var (
+	portNumber = flag.Int("p", 8080, "Port yang digunakan oleh aplikasi")
+)
+
+func main() {
+	// Parse flags
+	flag.Parse()
+
+	// Create backend
+	backEnd := NewBackEnd(*portNumber)
+	defer backEnd.Close()
+
+	// Serve app
+	backEnd.ServeApp()
+}
+
+func checkError(err error) {
+	if err != nil && err != sql.ErrNoRows {
+		panic(err)
+	}
+}
