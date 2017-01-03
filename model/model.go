e1fe380481d0b62e341feeee3268d010476c2dcc
Radhi Fadlillah <m.radhi.f@gmail.com>
2017-01-03 13:57:58 +0700

    New model for new API and template

--- model/model.go
+++ model/model.go
@@ -18,20 +18,27 @@ type Surat struct {
 	Nomor       string `db:"nomor"        json:"nomor"`
 	Perihal     string `db:"perihal"      json:"perihal"`
 	Sumber      string `db:"sumber"       json:"sumber"`
+	Tujuan      string `db:"tujuan"       json:"tujuan"`
+	Jabatan     string `db:"jabatan"      json:"jabatan"`
 	Tanggal     string `db:"tanggal"      json:"tanggal"`
 	WaktuTerima string `db:"waktu_terima" json:"waktuTerima"`
 	Prioritas   int    `db:"prioritas"    json:"prioritas"`
+	Status      int    `db:"status"       json:"status"`
+	Read        int    `db:"read"         json:"read"`
 }
 
 type Disposisi struct {
 	ID        int      `db:"id"        json:"id"`
 	SuratID   int      `db:"surat_id"  json:"suratId"`
 	ParentID  null.Int `db:"parent_id" json:"parentId"`
-	SumberID  null.Int `db:"sumber_id" json:"sumberId"`
-	TargetID  int      `db:"target_id" json:"targetId"`
+	SumberID  int      `db:"sumber_id" json:"sumberId"`
+	Sumber    string   `db:"sumber"    json:"sumber"`
+	Jabatan   string   `db:"jabatan"   json:"jabatan"`
+	TujuanID  int      `db:"tujuan_id" json:"tujuanId"`
 	Waktu     string   `db:"waktu"     json:"waktu"`
 	Status    int      `db:"status"    json:"status"`
 	Modified  string   `db:"modified"  json:"modified"`
+	Read      int      `db:"read"      json:"read"`
 	Deskripsi string   `db:"deskripsi" json:"deskripsi"`
 }
 
@@ -51,10 +58,16 @@ type UpdatePasswordRequest struct {
 	Password     string `json:"password"`
 }
 
+type NewSuratCount struct {
+	Status int `db:"status" json:"status"`
+	Count  int `db:"count"  json:"count"`
+}
+
 type PageListSurat struct {
-	Page    int     `json:"page"`
-	MaxPage int     `json:"maxPage"`
-	Item    []Surat `json:"item"`
+	Page     int             `json:"page"`
+	MaxPage  int             `json:"maxPage"`
+	NewCount []NewSuratCount `json:"newCount"`
+	Item     []Surat         `json:"item"`
 }
 
 type PageListAccount struct {
@@ -64,13 +77,15 @@ type PageListAccount struct {
 }
 
 type PageSurat struct {
-	Surat    Surat          `json:"surat"`
-	Timeline []TimelineItem `json:"timeline"`
+	Surat     Surat          `json:"surat"`
+	Disposisi Disposisi      `json:"disposisi"`
+	Files     []string       `json:"files"`
+	Timeline  []TimelineItem `json:"timeline"`
 }
 
 type TimelineItem struct {
 	ID       int            `db:"id"      json:"id"`
-	Target   string         `db:"target"  json:"target"`
+	Tujuan   string         `db:"tujuan"  json:"tujuan"`
 	Jabatan  string         `db:"jabatan" json:"jabatan"`
 	Waktu    string         `db:"waktu"   json:"waktu"`
 	Status   int            `db:"status"  json:"status"`
@@ -83,8 +98,37 @@ type TimelineStatus struct {
 	Children []TimelineStatus `json:"children"`
 }
 
+type DisposisiUpdate struct {
+	ID       int               `db:"id"      json:"id"`
+	Status   int               `db:"status"  json:"status"`
+	Nomor    string            `db:"nomor"   json:"nomor"`
+	Perihal  string            `db:"perihal" json:"perihal"`
+	Email    string            `db:"email"   json:"email"`
+	Telepon  string            `db:"telepon" json:"telepon"`
+	Children []DisposisiUpdate `json:"children"`
+}
+
 type EmailNewAccount struct {
 	Account  Account
 	Domain   string
 	Password string
+}
+
+type EmailNewSurat struct {
+	Surat  Surat
+	Domain string
+}
+
+type EmailNewDisposisi struct {
+	Sumber  string
+	Jabatan string
+	Surat   Surat
+	Domain  string
+}
+
+type EmailNewStatus struct {
+	Nomor   string
+	Perihal string
+	Status  int
+	Domain  string
 }
